d21e83bd6e35c8667a3ee70d26f9d2b9ceb6e750
mahdi aghasi <mahdi.aghasiwaz@gmail.com>
2025-08-24 22:53:55 +0330

    add class Book in project

---
+++ LibraryManager/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibraryManager
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
