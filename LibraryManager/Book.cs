c9edaa2083182fbd4f50754397dd1d67c4d262a9
mahdi aghasi <mahdi.aghasiwaz@gmail.com>
2025-08-25 22:54:38 +0330

    edit class Book

--- LibraryManager/Book.cs
+++ LibraryManager/Book.cs
@@ -7,6 +7,22 @@ namespace LibraryManager
         public string Title { get; set; }
         public string Author { get; set; }
         public string ISBN { get; set; }
-        public bool IsAvailable { get; set; }
+        public bool IsAvailable { get; set; }=true; 
+
+        public Book()
+        {
+        }
+
+        public Book(string title, string author, string iSBN)
+        {
+            Title = title;
+            Author = author;
+            ISBN = iSBN;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{Title}\t{Author}\t{ISBN}");
+        }
     }
 }
