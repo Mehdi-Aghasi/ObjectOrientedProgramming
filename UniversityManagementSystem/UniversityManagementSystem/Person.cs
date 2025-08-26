f3fe26debd737c5bc9fb387c0590064b39fd21ea
mahdi aghasi <mahdi.aghasiwaz@gmail.com>
2025-08-27 00:44:56 +0330

    Add class Person

---
+++ UniversityManagementSystem/UniversityManagementSystem/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UniversityManagementSystem
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public void GetDetails()
+        {
+            Console.WriteLine($"Name is {Name} and Age is {Age}");
+        }
+    }
+}
