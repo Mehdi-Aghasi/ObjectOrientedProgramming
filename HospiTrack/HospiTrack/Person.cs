daf0da52b17d93983a35d411393cf29f99df875c
mahdi aghasi <mahdi.aghasiwaz@gmail.com>
2025-08-29 21:14:55 +0330

    add class person

---
+++ HospiTrack/HospiTrack/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Cache;
+
+namespace HospiTrack
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Nationalld { get; set; }
+        public void GetDetails()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}, National ID: {Nationalld}");
+        }
+    }
+}
