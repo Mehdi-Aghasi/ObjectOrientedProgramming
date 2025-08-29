0d9fa54524f00305b240c4c706ca22d28643f608
mahdi aghasi <mahdi.aghasiwaz@gmail.com>
2025-08-29 19:17:08 +0330

    add class student in project

---
+++ UniversityManagementSystem/UniversityManagementSystem/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniversityManagementSystem
+{
+    class Student:Person
+    {
+        public int StudentId { get; set; }
+        public string Major { get; set; }
+        public override void GetDetails()
+        {
+
+            base.GetDetails();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"StudentId is {StudentId} and Major is {Major}");
+            Console.ResetColor();
+        }
+    }
+}
