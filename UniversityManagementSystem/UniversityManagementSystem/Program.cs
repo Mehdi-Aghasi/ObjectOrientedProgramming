8a472b174ac59fcdfcb5fb3afc654111e0a5c099
mahdi aghasi <mahdi.aghasiwaz@gmail.com>
2025-08-29 19:18:25 +0330

    start project

--- UniversityManagementSystem/UniversityManagementSystem/Program.cs
+++ UniversityManagementSystem/UniversityManagementSystem/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
 namespace UniversityManagementSystem
 {
@@ -10,6 +8,19 @@ namespace UniversityManagementSystem
     {
         static void Main(string[] args)
         {
+            List<Person> People = new List<Person>()
+            {
+                 new Student { StudentId = 1, Name = "Ali", Age = 20, Major = "Computer Science" },
+                 new Student { StudentId = 2, Name = "Sara", Age = 22, Major = "Mathematics" },
+                 new Professor { ProfessorId = 100, Name = "Dr. Reza", Age = 45, Subject = "Algorithms" },
+                 new Professor { ProfessorId = 101, Name = "Dr. Mina", Age = 38, Subject = "Database" }
+            };
+            foreach (var person in People)
+            {
+                person.GetDetails();
+                Console.WriteLine("*********************************************************");
+            }
+            Console.ResetColor();
         }
     }
 }
