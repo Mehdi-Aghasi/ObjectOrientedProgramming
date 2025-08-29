48aefda8eef37968daaca3655754b84992176594
mahdi aghasi <mahdi.aghasiwaz@gmail.com>
2025-08-29 20:20:06 +0330

    add class clothing in project

---
+++ OnlineStore/OnlineStore/Clothing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlineStore
+{
+    enum size
+    {
+        Small,
+        Medium,
+        Large,
+        LargeSmall,
+    }
+    class Clothing : Product
+    {
+        public string Material { get; set; }
+        public size Size { get; set; }
+        public override void GetProductDetails()
+        {
+            base.GetProductDetails();
+            Console.WriteLine($"Material=>{Material} and Size Is {Size}");
+        }
+    }
+}
