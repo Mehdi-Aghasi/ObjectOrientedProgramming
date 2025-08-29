cd45d10fd2e20fb98d1f125ab566323a52209272
mahdi aghasi <mahdi.aghasiwaz@gmail.com>
2025-08-29 19:16:44 +0330

    add person class in project

--- UniversityManagementSystem/UniversityManagementSystem/Person.cs
+++ UniversityManagementSystem/UniversityManagementSystem/Person.cs
@@ -8,7 +8,7 @@ namespace UniversityManagementSystem
     {
         public string Name { get; set; }
         public int Age { get; set; }
-        public void GetDetails()
+        public virtual void GetDetails()
         {
             Console.WriteLine($"Name is {Name} and Age is {Age}");
         }
