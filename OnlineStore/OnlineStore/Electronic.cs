5dbc98e7a1bcb80c62613274cdb6ed26098d764c
mahdi aghasi <mahdi.aghasiwaz@gmail.com>
2025-08-29 20:19:37 +0330

    add class electronic in project

---
+++ OnlineStore/OnlineStore/Electronic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlineStore
+{
+    class Electronic : Product, Idiscountable
+    {
+        public int WarrantyPeriod { get; set; }
+
+        public void ApplyDiscount(double Percent)
+        {
+            Price -= Price * Percent / 100;
+        }
+        public override void GetProductDetails()
+        {
+            base.GetProductDetails();
+            Console.WriteLine($"WarrantyPeriod:{WarrantyPeriod}");
+        }
+    }
+}
