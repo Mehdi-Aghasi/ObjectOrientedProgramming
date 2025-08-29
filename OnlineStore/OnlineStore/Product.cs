e13b62a8844448b60e4149c192d95c334dd15337
mahdi aghasi <mahdi.aghasiwaz@gmail.com>
2025-08-29 20:18:29 +0330

    add class product in project

---
+++ OnlineStore/OnlineStore/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlineStore
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public virtual void GetProductDetails()
+        {
+            Console.WriteLine($"NameProduct is {Name} and PriceProduct is {Price}");
+        }
+    }
+}
