c708e560f78d2a4a76b292b4074f7c9d2ba315c7
mahdi aghasi <mahdi.aghasiwaz@gmail.com>
2025-08-29 19:17:46 +0330

    add class professor in project

---
+++ UniversityManagementSystem/UniversityManagementSystem/Professor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Permissions;
+
+namespace UniversityManagementSystem
+{
+    class Professor:Person
+    {
+        public int ProfessorId { get; set; }
+        public string Subject { get; set; }
+        public override void GetDetails()
+        {
+            base.GetDetails();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"ProfessorId is {ProfessorId} and Subject is {Subject}");
+            Console.ResetColor();
+        }
+    }
+}
